9180c9a7ccb3ec941fad1d5aad94f56409576ba4
Jan-Christopher Pien <j.pien@searchmetrics.com>
2018-12-19 15:44:11 +0100

    LockedDropout handle packed sequences

--- locked_dropout.py
+++ locked_dropout.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 from torch.autograd import Variable
+from torch.nn.utils.rnn import PackedSequence, pad_packed_sequence, pack_padded_sequence
+
 
 class LockedDropout(nn.Module):
     def __init__(self):
@@ -8,7 +10,13 @@ class LockedDropout(nn.Module):
     def forward(self, x, dropout=0.5):
         if not self.training or not dropout:
             return x
+        sequence_lengths = None
+        if isinstance(x, PackedSequence):
+            x, sequence_lengths = pad_packed_sequence(x)
         m = x.data.new(1, x.size(1), x.size(2)).bernoulli_(1 - dropout)
         mask = Variable(m, requires_grad=False) / (1 - dropout)
         mask = mask.expand_as(x)
-        return mask * x
+        result = mask * x
+        if sequence_lengths is not None:
+            result = pack_padded_sequence(result, sequence_lengths)
+        return result
